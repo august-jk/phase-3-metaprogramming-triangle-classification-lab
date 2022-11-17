25e32d3d5f73c764a7c71a1bfd87ddbd36d9cd8d
Aletha Wheeless <augustkama98@gmail.com>
2022-11-17 13:49:23 -1000

    commit tree d05034da1951ce8b8ed2d1cb76338947391859be

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_reader :side1, :side2, :side3, :all_sides
+  
+  def initialize side1, side2, side3
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @all_sides = [side1, side2, side3].sort
+  end
+
+  def kind
+    if @all_sides[0] <= 0 || @all_sides[2] >= @all_sides[1] + @all_sides[0]
+      raise TriangleError
+    elsif @all_sides.uniq.length == 1
+      :equilateral
+    elsif @all_sides.uniq.length == 2
+      :isosceles
+    elsif @all_sides.uniq.length == 3
+      :scalene
+    end
+    
+  end
+
+  class TriangleError < StandardError
+  end
 end
+
+# e = Triangle.new(5, 5, 5)
+# i = Triangle.new(5, 5, 4)
+# s = Triangle.new(5, 4, 3)
+
+# binding.pry
+
+# 0
